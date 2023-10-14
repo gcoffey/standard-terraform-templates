1ecc82e1bf6a3e0b6ab253e2adc0e95d97313a32
root <root@localhost.localdomain>
2023-10-14 22:14:06 +0100

    Added azure_sandbox service pack Added automation_account, mssql_server azure resources

--- scripts/create-resource-templates.sh
+++ scripts/create-resource-templates.sh
@@ -7,6 +7,7 @@ RESOURCE_PLAYBOOK="./playbooks/create-tf-resource-template.yml"
 
 PROVIDER="${1}"
 RESOURCE="${2}"
+ARGS="${3}"
 
 if [[ "${PROVIDER}" != "" && "${RESOURCE}" != "" ]];
 then
@@ -14,7 +15,7 @@ then
   if [[ -d "${RESOURCE_DIR}" ]];
   then
     echo -e "Processing ${RESOURCE_DIR}"
-    ansible-playbook ${RESOURCE_PLAYBOOK} --extra-vars "provider_name=${PROVIDER} resource_name=${RESOURCE}"
+    ansible-playbook ${RESOURCE_PLAYBOOK} --extra-vars "provider_name=${PROVIDER} resource_name=${RESOURCE}" ${ARGS}
   fi
   exit 0
 fi
@@ -29,7 +30,7 @@ do
     if [[ -d "${RESOURCE_DIR}/${r}" ]];
     then
       echo -e "Processing ${RESOURCE_DIR}/${r}"
-      ansible-playbook ${RESOURCE_PLAYBOOK} --extra-vars "provider_name=${p} resource_name=${r}"
+      ansible-playbook ${RESOURCE_PLAYBOOK} --extra-vars "provider_name=${p} resource_name=${r}" ${ARGS}
     fi
   done
 
