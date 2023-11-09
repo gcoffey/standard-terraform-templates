a2484b0ac3f79eac759bfd217e56da9d6110ca5f
gcoffey <gareth@cachesure.co.uk>
2023-11-09 15:28:55 +0000

    Added Alicloud, alicloud_instance

--- scripts/create-json.sh
+++ scripts/create-json.sh
@@ -9,28 +9,36 @@ JSON_DIR="./json"
 PROVIDER_FILE="${JSON_DIR}/providers.json"
 SP_FILE="${JSON_DIR}/sp_providers.json"
 
-# Provider List
-echo "{ \"providers\": [ " > ${PROVIDER_FILE}; for p in `ls -1 ${PROVIDER_DIR}`; do echo "\"$p\"," >> ${PROVIDER_FILE}; done; echo " ] }" >> ${PROVIDER_FILE}
+if [[ -d "${PROVIDER_DIR}" ]];
+then
+  # Provider List
+  echo "{ \"providers\": [ " > ${PROVIDER_FILE}; for p in `ls -1 ${PROVIDER_DIR}`; do echo "\"$p\"," >> ${PROVIDER_FILE}; done; echo " ] }" >> ${PROVIDER_FILE}
 
-for p in `ls -1 ${PROVIDER_DIR}`;
-do
-  RESOURCE_DIR="${PROVIDER_DIR}/${p}"
-  RESOURCE_FILE="${JSON_DIR}/${p}_resources.json"
-  SP_RESOURCE_DIR="${SP_DIR}/${p}"
+  for p in `ls -1 ${PROVIDER_DIR}`;
+  do
+    RESOURCE_DIR="${PROVIDER_DIR}/${p}"
+    RESOURCE_FILE="${JSON_DIR}/${p}_resources.json"
+    SP_RESOURCE_DIR="${SP_DIR}/${p}"
 
-  # Resource List
-  echo -e "{  \n\"resources\": [ \n" > ${RESOURCE_FILE} ; 
-  for i in `ls -1 ${RESOURCE_DIR}`; 
-  do echo "\"$i\"," >> ${RESOURCE_FILE}; 
-  done; 
-  echo -e " ]\n}" >> ${RESOURCE_FILE}
-
-  # Service Pack List
-  echo -e "{  \n\"servicepacks\": [ \n" >> ${RESOURCE_FILE}
-  for p in `ls -1 ${SP_RESOURCE_DIR}`; 
-  do 
-    echo "\"$p\"," >> ${RESOURCE_FILE};
-  done; 
-  echo -e " ]\n}" >> ${RESOURCE_FILE}
-
-done
+    # Resource List
+    if [[ -d "${RESOURCE_DIR}" ]];
+    then
+      echo -e "{  \n\"resources\": [ \n" > ${RESOURCE_FILE} ; 
+      for i in `ls -1 ${RESOURCE_DIR}`; 
+      do 
+        echo "\"$i\"," >> ${RESOURCE_FILE}; 
+      done; 
+      echo -e " ]\n}" >> ${RESOURCE_FILE}
+    fi
+    if [[ -d "${SP_RESOURCE_DIR}" ]];
+    then
+      # Service Pack List
+      echo -e "{  \n\"servicepacks\": [ \n" >> ${RESOURCE_FILE}
+      for p in `ls -1 ${SP_RESOURCE_DIR}`; 
+      do 
+        echo "\"$p\"," >> ${RESOURCE_FILE};
+      done; 
+      echo -e " ]\n}" >> ${RESOURCE_FILE}
+    fi
+  done
+fi
